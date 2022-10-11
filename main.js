274e2f58ca3fa33d6a22070916d3a3929fba8212
Mausumibera <82108381+Mausumibera@users.noreply.github.com>
2022-10-11 19:39:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+    Canvas=createCanvas(300,300);
+    canvas.center();
+}
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("MY_FILTER_IMAGE.png");
+}
